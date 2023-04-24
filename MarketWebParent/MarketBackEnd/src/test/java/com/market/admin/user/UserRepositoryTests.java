1100f0e8fdfdb59f6d99c309529b7a39aadb0eb7
Mainuddin <web97development@gmail.com>
2023-04-24 22:11:36 +0530

    users pagination added

--- MarketWebParent/MarketBackEnd/src/test/java/com/market/admin/user/UserRepositoryTests.java
+++ MarketWebParent/MarketBackEnd/src/test/java/com/market/admin/user/UserRepositoryTests.java
@@ -8,14 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.test.annotation.Rollback;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@DataJpaTest
+@DataJpaTest(showSql = false)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Rollback(false)
 public class UserRepositoryTests {
@@ -109,5 +113,21 @@ public class UserRepositoryTests {
     public void testEnabledUser(){
         Integer id =  1;
         repo.updateEnabledStatus(id,true);
+    }
+
+    @Test
+    public void testListFirstPage(){
+        int pageNumber = 1;
+        int pageSize = 4;
+
+        Pageable pageable = PageRequest.of(pageNumber,pageSize);
+        Page<User> page = repo.findAll(pageable);
+
+        List<User> listUsers = page.getContent();
+
+        listUsers.forEach(user -> System.out.println(user));
+
+        assertThat(listUsers.size()).isEqualTo(pageSize);
+
     }
 }

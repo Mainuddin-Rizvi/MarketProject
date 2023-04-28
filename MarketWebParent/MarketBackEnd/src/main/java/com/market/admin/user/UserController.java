068f47d23f399c898a8f51ba4aabafe1a73ab1cc
Mainuddin <web97development@gmail.com>
2023-04-28 21:29:09 +0530

    code filter search functionality

--- MarketWebParent/MarketBackEnd/src/main/java/com/market/admin/user/UserController.java
+++ MarketWebParent/MarketBackEnd/src/main/java/com/market/admin/user/UserController.java
@@ -27,13 +27,16 @@ public class UserController {
 
     @GetMapping("/users")
     public String listFirstPage(Model model) {
-        return listByPage(1, model,"firstName","lastName");
+        return listByPage(1, model,"firstName","lastName",null);
     }
 
     @GetMapping("/users/page/{pageNum}")
-    public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model
-    , @Param("sortField") String sortField, @Param("sotDir") String sortDir){
-        Page<User> page = service.listByPage(pageNum,sortField,sortDir);
+    public String listByPage(
+            @PathVariable(name = "pageNum") int pageNum, Model model,
+            @Param("sortField") String sortField,
+            @Param("sotDir") String sortDir,
+            @Param("keyword") String keyword){
+        Page<User> page = service.listByPage(pageNum,sortField,sortDir,keyword);
         List<User> listUsers = page.getContent();
 
         long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
@@ -54,6 +57,8 @@ public class UserController {
         model.addAttribute("sortField", sortField);
         model.addAttribute("sortDir", sortDir);
         model.addAttribute("reverseSortDir", reverseSortDir);
+        model.addAttribute("keyword", keyword);
+
 
 
 

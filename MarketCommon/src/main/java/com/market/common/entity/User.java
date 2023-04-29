c7556a2e7eefdb55525a95d7bdb1d321ba929d59
Mainuddin <web97development@gmail.com>
2023-04-29 13:16:34 +0530

    responsive according to mobile

--- MarketCommon/src/main/java/com/market/common/entity/User.java
+++ MarketCommon/src/main/java/com/market/common/entity/User.java
@@ -109,6 +109,10 @@ public class User {
 
         return "/user-photos/" + this.id + "/" + this.photos;
     }
+    @Transient
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
 
 
 }

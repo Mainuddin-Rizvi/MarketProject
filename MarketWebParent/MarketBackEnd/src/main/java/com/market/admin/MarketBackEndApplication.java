2445220b8f1d9f8f2ae8a05a627789d83eb2cbb3
Mainuddin <web97development@gmail.com>
2023-05-05 23:51:57 +0530

    logger configurere slf4j

--- MarketWebParent/MarketBackEnd/src/main/java/com/market/admin/MarketBackEndApplication.java
+++ MarketWebParent/MarketBackEnd/src/main/java/com/market/admin/MarketBackEndApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan({"com.market.common.entity","com.market.admin.user"})
+@EntityScan({"com.market.common.entity"})
 public class MarketBackEndApplication {
 
 	public static void main(String[] args) {

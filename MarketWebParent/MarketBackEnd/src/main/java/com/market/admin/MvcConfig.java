1a0ab5e15e252b34808d3d564cd461ec8521400d
Mainuddin <web97development@gmail.com>
2023-05-16 20:16:59 +0530

    code update category or edit

--- MarketWebParent/MarketBackEnd/src/main/java/com/market/admin/MvcConfig.java
+++ MarketWebParent/MarketBackEnd/src/main/java/com/market/admin/MvcConfig.java
@@ -19,6 +19,8 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/" + dirName + "/**")
                 .addResourceLocations("file:" + userPhotosPath + "/");
 
+
+
         String categoryImagesDirName = "category-images";
         Path categoryImagesDir = Paths.get(categoryImagesDirName);
 

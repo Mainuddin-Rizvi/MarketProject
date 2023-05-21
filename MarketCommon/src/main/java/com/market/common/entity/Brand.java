95716e46584ca94ee76a7dccb1f27d94456c1a9f
Mainuddin <web97development@gmail.com>
2023-05-21 20:39:43 +0530

    brand repository test

--- MarketCommon/src/main/java/com/market/common/entity/Brand.java
+++ MarketCommon/src/main/java/com/market/common/entity/Brand.java
@@ -25,6 +25,15 @@ public class Brand {
     )
     private Set<Category> categories = new HashSet<>();
 
+    public Brand() {
+
+    }
+
+    public Brand(String name) {
+        this.name = name;
+        this.logo = "brand-logo.png";
+    }
+
     public Integer getId() {
         return id;
     }
@@ -55,6 +64,11 @@ public class Brand {
 
     public void setCategories(Set<Category> categories) {
         this.categories = categories;
+    }
+
+    @Override
+    public String toString() {
+        return "Brand [id=" + id + ", name=" + name + ", categories=" + categories + "]";
     }
 
 

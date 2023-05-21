49b255eeb21bf1a396b84d2c3f0dac311d91ca25
Mainuddin <web97development@gmail.com>
2023-05-21 16:32:31 +0530

    code brand entity relationship

---
+++ MarketWebParent/MarketBackEnd/src/main/java/com/market/admin/brand/BrandRepository.java
@@ -0,0 +1,8 @@
+package com.market.admin.brand;
+
+import com.market.common.entity.Brand;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer> {
+
+}
